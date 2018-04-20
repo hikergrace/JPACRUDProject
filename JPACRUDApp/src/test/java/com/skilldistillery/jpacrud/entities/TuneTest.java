f8eb8dfdf8920bae83f45bfa67d40708762c7532
Grace Carter <graciec@gmail.com>
2018-04-20 15:12:30 -0600

    Created JUnit test

--- JPACRUDApp/src/test/java/com/skilldistillery/jpacrud/entities/TuneTest.java
+++ JPACRUDApp/src/test/java/com/skilldistillery/jpacrud/entities/TuneTest.java
@@ -1,24 +1,45 @@
 package com.skilldistillery.jpacrud.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class TuneTest {
-
+	private EntityManagerFactory emf;
+	private EntityManager em; 
+	
+	
 	@BeforeEach
 	void setUp() throws Exception {
+		emf = Persistence.createEntityManagerFactory("CrudApp");
+		em = emf.createEntityManager();
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
 	}
-
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("Test Tune Entity Mapping")
+	void test_tune_entity_mapping() {
+		Tune t = em.find(Tune.class, 1);
+		assertEquals("Pull the Knife and Stick it Again", t.getName());
+		assertEquals("Jig", t.getRhythm());
+		assertEquals("Emin", t.getKey());
+		assertEquals("Irish", t.getProvenance());
+		
 	}
+
+//	@Test
+//	void test() {
+//		fail("Not yet implemented");
+//	}
 
 }
